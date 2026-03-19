0e088f3e7d1ba01c016168f3e510b9c4be86c56f
MariaSBotelho <mariaclarasilvabotelho27@gmail.com>
2026-03-19 12:58:11 -0300

    continuação da atividade

--- atividade3.js
+++ atividade3.js
@@ -25,3 +25,37 @@ console.log(`É maior de idade? ${maiorDeIdade(18)}`);
 //(uma palavra que é lida da mesma forma de trás para frente) utilizando o método de string reverse().
 //Retorne true se for um palíndromo e false caso contrário. Imprima o resultado no console.
 
+function verificaPalindromo(letra){
+
+    if(letra.toLowerCase() === letra.toLowerCase().split('').reverse().join('')){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(verificaPalindromo('arara')); //true 
+console.log(verificaPalindromo('computador')); //false
+
+//split('')= Transforma a string em um Array de letras (ex: ['a', 'b'])
+//toLowerCase()= Padroniza a string para letras minúsculas.
+//reverse()= Inverte a ordem dos elementos dentro do Array.
+//join('')= Pega o Array invertido e o transforma em string novamente.
+
+//4-Crie uma função que receba três números como parâmetros e determine qual é o maior entre eles.
+//Utilize estruturas condicionais (if, else) para comparar os valores e determinar o maior.
+//Imprima o maior valor no console.
+
+function verificaMaior(a , b , c){
+    if(a > b > c){
+        return 'A é maior';
+    } else if(b > a > c){
+        return 'B é maior';
+    } else if(c > a > b){
+        return ''
+    }
+}
+
+
+
+//5-Crie uma arrow function chamada calculaPotencia que receba dois parâmetros: a base e o expoente.
+//A função deve calcular a potência da base elevada ao expoente e retornar o resultado.
