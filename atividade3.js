e9fb0568b1c1224b4158ab033cc6846290d9597f
MariaSBotelho <mariaclarasilvabotelho27@gmail.com>
2026-03-20 12:06:26 -0300

    Curso finalizado!!

--- atividade3.js
+++ atividade3.js
@@ -46,16 +46,36 @@ console.log(verificaPalindromo('computador')); //false
 //Imprima o maior valor no console.
 
 function verificaMaior(a , b , c){
-    if(a > b > c){
+    if(typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number'){
+        return 'Insira apenas números';
+    }
+
+    if(a > b && a> c){
         return 'A é maior';
-    } else if(b > a > c){
+    } else if(b > a && b > c){
         return 'B é maior';
-    } else if(c > a > b){
-        return ''
+    } else if(c > a && c > b){
+        return 'C é maior';
+    } else{
+        return 'Tem números iguais.';
     }
 }
-
-
+let maria;
+console.log(verificaMaior(2,5,6));
+console.log(verificaMaior(2,7,6));
+console.log(verificaMaior(10,7,6));
+console.log(verificaMaior(2,2,2));
+console.log(verificaMaior(maria));
 
 //5-Crie uma arrow function chamada calculaPotencia que receba dois parâmetros: a base e o expoente.
 //A função deve calcular a potência da base elevada ao expoente e retornar o resultado.
+
+const calculaPotencia = (base,expoente) =>{
+    return Math.pow(base,expoente);
+}
+const base = 2;
+const expoente = 5
+const resultado = calculaPotencia(base,expoente);
+
+//console.log(calculaPotencia(2,5));
+console.log(`O resultado de ${base} elevado a ${expoente} é: ${resultado}`)
